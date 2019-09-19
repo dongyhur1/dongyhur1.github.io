1c67f8faca3813f743ff175b26f9ed520d9846f5
dongyhur1 <48274676+dongyhur1@users.noreply.github.com>
2019-09-19 15:46:15 +0900

    Create 숙제 EX1_9.java

---
+++ 숙제 EX1_9.java
@@ -0,0 +1,16 @@
+숙제 EX1_9.java
+
+pacakage org.javaro.lecture;
+public class Ex1_9{
+ static double PI = 3.141592;
+ public static void main(String [] args){
+ System.out.println("원주율"+PI+"20XX0901"+"변수");
+ int r =15;// int 정수형, 이름:5, 15대입
+ System.out.println("원주율"+PI+",반지름"+r+"cm인 원의 둘레는 "+2*PI*r+"cm 입니다");
+ System.out.println("2진수:"+lnteger.toBinaryString(r));
+ System.out.println("8진수:"+lnteger.toOctalString(r));
+ System.out.println("16진수:"+lnteger.toHexString(r));
+ double rd= 15.3; // double 실수형, 이름:rd, 15.3대입
+ System.out.println("원주율"+PI+,"반지름"+r+"cm인 원의 면적은"+r*PI*r+"cmcm입니다");
+}
+}
